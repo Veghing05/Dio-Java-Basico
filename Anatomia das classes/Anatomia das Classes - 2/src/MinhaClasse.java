e3a03201e22e6e71ded97cdacb0c02128a5ab4b1
Veghing05 <renatosbv27@gmail.com>
2026-05-24 18:23:29 -0300

    Aula02

---
+++ Anatomia das classes/Anatomia das Classes - 2/src/MinhaClasse.java
@@ -0,0 +1,22 @@
+public class MinhaClasse {
+  public static void main(String[] args)  {
+
+    int ano = 2026;
+
+    ano = 2027;
+
+    final String BR = "Brasil";
+
+    BR = "Brazil";
+
+    double PI = 3.14;
+
+    int ESTADOS_BRASILEIROS = 27;
+
+    int ANO_2000 = 2000;
+
+    int numero_um = 1;
+
+
+  }
+  }
