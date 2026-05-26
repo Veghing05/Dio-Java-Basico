88833ed643346896cb5e4bb26fb0a16fbbcbfb99
Veghing05 <renatosbv27@gmail.com>
2026-05-26 19:26:31 -0300

    Aula 04 - Identação

---
+++ Anatomia das classes/Anatomia das Classes - 4/BoletimEstudantil.java
@@ -0,0 +1,27 @@
+public class BoletimEstudantil {
+  
+  public static void main(String[] args){
+    int mediaFinal = 6;
+    if(mediaFinal<6)
+    System.out.println("REPROVADO");
+    else if(mediaFinal==6)
+    System.out.println("PROVA MINERVA");
+    else
+    System.out.println("APROVADO");
+
+  }
+}
+
+public class BoletimEstudantil {
+
+  public static void main(String[] args){
+    int mediaFinal = 7;
+    if(mediaFinal<6)
+      System.out.println("REPROVADO");
+    else if (mediaFinal==6)
+      System.out.println("PROVA MINERVA");
+    else
+      System.out.println("APROVADO");
+
+  }
+}
