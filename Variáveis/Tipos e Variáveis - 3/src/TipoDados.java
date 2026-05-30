6709a618b446b8e5e7bbcfc80f045e7d870578ef
Veghing05 <renatosbv27@gmail.com>
2026-05-30 15:41:17 -0300

    Tipos primitivos

---
+++ Variáveis/Tipos e Variáveis - 3/src/TipoDados.java
@@ -0,0 +1,15 @@
+public class TipoDados {
+    public static void main(String[] args){
+        //byte idade = 123;
+       // short ano = 2021;
+       // int cep = 21070333;
+       // long cpf = 98765432109L;
+       // float pi = 3.14F;
+       // double salario = 1275.33;
+
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       short numeroCurto2 = (short) numeroNormal;
+
+    }
+}
