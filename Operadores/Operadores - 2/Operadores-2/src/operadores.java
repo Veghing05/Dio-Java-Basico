da5d922583a125dec66fbeeecc037805efe9c6fb
Veghing05 <renatosbv27@gmail.com>
2026-05-30 18:06:03 -0300

    Operadores - 2

---
+++ Operadores/Operadores - 2/Operadores-2/src/operadores.java
@@ -0,0 +1,47 @@
+public class operadores {
+    public static void main(String[] args) throws Exception {
+  /***   String nome = "RENATO";  
+    int idade = 22;
+    double peso = 68.5;
+    char sexo = 'M';
+    boolean doadorOrgao = false;
+    Date dataNascimento = new Date();
+        
+    System.out.println("Hello, World!");
+
+//Aritmética
+
+    double soma = 10.5 + 15.7;
+    int subtração = 113 - 25;
+    int multiplicação = 20 * 7;
+    int divisão = 15 / 3;
+    int modulo = 18 % 3;
+    double resultado = (10 * 7) + (20/4); ***/
+
+    /*** String nomeCompleto = "LINGUAGEM" + "JAVA";
+    
+    System.out.println(nomeCompleto); ***/
+
+        String concatenacao="?";
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        
+        System.out.println(concatenacao);
+    }
+}
---
+++ Operadores/README.md
@@ -0,0 +1,17 @@
+## Objetivo d curso
+
+Apresentar o significado dos símbolos especiais de acordo com a linguagem, de programação Java.
+
+Pré-requisitos 
+
+° Diretorio do projeto definido
+° Integração com Github
+° Noções em declaração de variáveis.
+
+Assuntos
+
+° Operadores aritméticos
+° Operadores unários
+° Operdaores relacionais
+° Operadores lógicos
+° Operador ternário
